826198604dd68d130f918691a0e4a95bb864d08f
Mari <dennismaricarl@gmail.com>
2023-10-26 17:48:17 -0700

    folders set up

---
+++ SERVER/app.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+
+//logging middleware 
+app.use(morgan("dev"));
+
+//Body parsing middleware 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(path.join(_dirname, '../../dist')))
+
+module.exports = app; 
