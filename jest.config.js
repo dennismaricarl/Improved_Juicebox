e64182674ad6f1d681e78c2b263277906f69f97c
Mari <dennismaricarl@gmail.com>
2023-11-01 20:53:13 -0700

    testing set up added

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    clearMocks: true,
+
+    setupFilesAfterEnv: ['<rootDir>/mocks/prismaMocks.js']
+};
---
+++ mocks/prismaMock.js
@@ -0,0 +1,11 @@
+const {mockDeep, mockReset} = require('jest-mock-extended')
+
+const prisma = require('../server/db/client')
+const prismaMock = prisma
+jest.mock('../server/db/client', () => mockDeep())
+
+beforeEach(() => {
+    mockReset(prismaMock)
+})
+
+module.exports = prismaMock 
